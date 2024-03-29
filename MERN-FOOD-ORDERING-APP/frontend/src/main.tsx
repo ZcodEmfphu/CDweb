99e2f4d51bad0c50494641af5281c10902dbeb4b
MinhPhu <minhphuzcod@gmail.com>
2024-03-29 23:43:12 +0700

    Lam dc mot xiu

--- MERN-FOOD-ORDERING-APP/frontend/src/main.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRoutes';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./globals.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-   <Router>
-    <AppRoutes />
-   </Router>
-  </React.StrictMode>,
+    <Router>
+      <AppRoutes />
+    </Router>
+  </React.StrictMode>
 );

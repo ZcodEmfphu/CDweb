4d1c61ebfc9c88cf75f9eaa64df2fd9a8e91141e
MinhPhu <minhphuzcod@gmail.com>
2024-03-31 20:46:03 +0700

    landing page

--- MERN-FOOD-ORDERING-APP/frontend/src/components/MobilleNav.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/components/MobilleNav.tsx
@@ -13,7 +13,7 @@ const MobileNav = () => {
   return (
     <Sheet>
       <SheetTrigger>
-        <Menu className="text-orange-500" />
+        <Menu className="text-blue-500" />
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
@@ -21,7 +21,7 @@ const MobileNav = () => {
         </SheetTitle>
         <Separator />
         <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-orange-500">Log in</Button>
+          <Button className="flex-1 font-bold bg-blue-500">Log in</Button>
         </SheetDescription>
       </SheetContent>
     </Sheet>

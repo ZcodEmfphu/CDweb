268c984d34af1fca008dcc251c535e364e4e9b0f
MinhPhu <minhphuzcod@gmail.com>
2024-04-01 23:43:34 +0700

    auth0 setup

--- MERN-FOOD-ORDERING-APP/frontend/src/AppRoutes.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/AppRoutes.tsx
@@ -5,7 +5,14 @@ import HomePage from "./pages/HomePage";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout><HomePage/></Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- MERN-FOOD-ORDERING-APP/frontend/src/components/MainNav.tsx
+++ MERN-FOOD-ORDERING-APP/frontend/src/components/MainNav.tsx
@@ -1,6 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <Button
       variant="ghost"

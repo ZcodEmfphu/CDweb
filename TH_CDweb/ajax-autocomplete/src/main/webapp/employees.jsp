fcbca9c3115bfed611e7e6a7066155d940980e6a
MinhPhu <minhphuzcod@gmail.com>
2024-04-07 21:09:11 +0700

    lab1

---
+++ TH_CDweb/ajax-autocomplete/src/main/webapp/employees.jsp
@@ -0,0 +1,26 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<head>
+</head>
+<body>
+ <h1>Search Results</h1>
+ <hr/>
+ 
+<c:choose>
+ <c:when test="${requestScope.employees == null}">
+  <p><font size="+2" color="red">Unable to locate any employees.</font></p>
+ </c:when>
+ <c:otherwise>
+   <c:forEach var="employee" begin="0" items="${requestScope.employees}">
+   <p><a href="autocomplete?action=lookup&id=${employee.id}">${employee.firstName} ${employee.lastName}</a></p>
+  </c:forEach>
+ </c:otherwise>
+</c:choose>
+<br/>
+ <p>
+  <a href="index.jsp">Go back to the application home</a>.
+ </p>
+ </body>
+</html>
+
---
+++ TH_CDweb/ajax-validation/src/main/webapp/scripts/jquery-ajax.js
@@ -0,0 +1,28 @@
+$(function() {
+	$("#userid").keyup(validateUserId);
+	disableSubmitBtn();
+});
+
+function validateUserId() {
+	var url = "validate?id=" + $("#userid").val();
+	$.ajax({
+		url: url, success: processRequest, cache: false
+	});
+}
+
+function processRequest(text) { 
+	var xmlDoc = $.parseXML(text),
+	    valid = $(xmlDoc).find("valid");
+	if (valid == "false") {
+		$("#userIdMessage").html("<div style=\"color:red\">Invalid User Id</div>");
+		$("#submit_btn").prop("disabled", true);
+	} else {
+		$("#userIdMessage").html("<div style=\"color:green\">Valid User Id</div>");
+		$("#submit_btn").prop("disabled", false);
+	}
+}
+
+function disableSubmitBtn() {
+	$("#submit_btn").prop("disabled", true);
+}
+

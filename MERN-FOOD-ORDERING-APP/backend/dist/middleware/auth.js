5c04d08aa1261a2706043aecbbb340629af9aac4
MinhPhu <minhphuzcod@gmail.com>
2024-05-20 18:25:57 +0700

    bug

--- MERN-FOOD-ORDERING-APP/backend/dist/middleware/auth.js
+++ MERN-FOOD-ORDERING-APP/backend/dist/middleware/auth.js
@@ -13,9 +13,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.jwtParse = exports.jwtCheck = void 0;
-const express_oauth2_jwt_bearer_1 = require("express-oauth2-jwt-bearer");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const user_1 = __importDefault(require("../model/user"));
+const express_oauth2_jwt_bearer_1 = require("express-oauth2-jwt-bearer");
 exports.jwtCheck = (0, express_oauth2_jwt_bearer_1.auth)({
     audience: process.env.AUTH0_AUDIENCE,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
